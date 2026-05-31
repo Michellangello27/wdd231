6fc3394da7fe8bdca0f1ed6e84416c56ef1baa08
Michellangello27 <angelcornejo@gmail.com>
2026-05-30 19:52:55 -0500

    improvements and changes in date format

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,27 @@
+/* ............. THANK YOU PAGE ............. */
+
+const params = new URLSearchParams(window.location.search);
+const timestamp = params.get("timestamp");
+
+document.querySelector("#firstName").textContent =
+    params.get("firstName") || "Not provided";
+
+document.querySelector("#lastName").textContent =
+    params.get("lastName") || "Not provided";
+
+document.querySelector("#email").textContent =
+    params.get("email") || "Not provided";
+
+document.querySelector("#phone").textContent =
+    params.get("phone") || "Not provided";
+
+document.querySelector("#organization").textContent =
+    params.get("organization") || "Not provided";
+
+document.querySelector("#membership").textContent =
+    params.get("membershipLevel") || "Not provided";
+
+document.querySelector("#timestamp").textContent =
+    timestamp
+        ? new Date(timestamp).toLocaleString()
+        : "Not provided"; 
